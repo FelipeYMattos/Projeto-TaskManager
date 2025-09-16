d8fdb28848f88fcec555b90624b0d8a6fb963293
User <vitohugocoleta2005@gmail.com>
2025-09-16 17:52:35 -0300

    Adicionado @CrossOrigin nos métodos de  Controller para lidar com CORS

--- task-manager/src/main/java/com/taskManager/task_manager/controller/UsuarioController.java
+++ task-manager/src/main/java/com/taskManager/task_manager/controller/UsuarioController.java
@@ -15,28 +15,33 @@ public class UsuarioController {
 
     private final UsuarioService service;
 
+    @CrossOrigin
     @GetMapping
     public ResponseEntity<UsuarioEntity> getUsuarioPorId(@RequestParam Integer id){
         return ResponseEntity.ok(service.getUsuarioPorId(id));
     }
 
+    @CrossOrigin
     @GetMapping("/all")
     public ResponseEntity<List<UsuarioEntity>> getTodosUsuarios(){
         return ResponseEntity.ok(service.getTodosUsuarios());
     }
 
+    @CrossOrigin
     @PostMapping
     public ResponseEntity<Void> postUsuario(@RequestBody UsuarioEntity usuario){
         service.postUsuario(usuario);
         return ResponseEntity.ok().build();
     }
 
+    @CrossOrigin
     @DeleteMapping
     public ResponseEntity<Void> deleteUsuarioPorId(@RequestParam Integer id){
         service.deleteUsuarioPorId(id);
         return ResponseEntity.ok().build();
     }
 
+    @CrossOrigin
     @PutMapping
     public ResponseEntity<Void> putUsuario(@RequestParam Integer id, @RequestBody UsuarioEntity usuario){
         service.putById(id, usuario);
